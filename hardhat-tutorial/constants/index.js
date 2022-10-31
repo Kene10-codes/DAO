191ae97ed6c35d221b878e4ae943f05ac15e9f5a
keneNwobodo <nwobodokenechukwu2@gmail.com>
2022-10-31 07:54:14 +0100

    deployed DAO app

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,7 @@
+// Replace the value with your NFT contract address
+const CRYPTODEVS_NFT_CONTRACT_ADDRESS =
+  "0x4C914A38d80409df00de4989Bbc45393c6417E36";
+
+module.exports = { CRYPTODEVS_NFT_CONTRACT_ADDRESS };
+
+// FAKE NFT  0x88A21a4e1ef63c1E85854297e45f602Ffbe9B19c
